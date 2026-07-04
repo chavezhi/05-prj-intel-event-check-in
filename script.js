e355a6911728416e6f2be986b43eb81bdc721d7b
chavezhi <chavezhi@appstate.edu>
2026-07-04 17:16:39 +0000

    Implement form submission handling for check-in process

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get all needed DOM elements
+const form = document.getElementById("checkInForm");
+const nameInput = document.getElementById("attendeeName");
+const teamSelect = document.getElementById("teamSelect");
+
+// Handle form submission
+form.addEventListener("submit", function (e) {
+  event.preventDefault();
+
+  // Get form values
+  const name = nameInput.value;
+  const team = teamSelect.value;
+
+  console.log(name, team)
+})
